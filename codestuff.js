c98683905d393e5a1f3c58c1919b6cdd2dbc54cd
Aidan Willett <Gamer9310@gmail.com>
2020-01-08 17:23:24 -0800

    first commit

---
+++ codestuff.js
@@ -0,0 +1,9 @@
+const saveBttn = document.querySelector(".save-note")
+const saveBttn = document.querySelector(".new-note")
+
+saveBttn.addEventListener("click", myfunction);
+
+myfunction(){
+    console.log("hi ");
+    
+}
